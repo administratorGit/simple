bbbc44a9c2eff14be1417ae7004826f18c8c1d09
weiy <ye_wei321@163.com>
2018-11-20 14:05:13 +0800

    idea git test

---
+++ src/main/java/tk/mybatis/util/StringUtil.java
@@ -0,0 +1,18 @@
+package tk.mybatis.util;
+
+public class StringUtil {
+	
+	public static boolean isEmpty(String str){
+		return str == null || str.length() == 0;
+	}
+	
+	public static boolean isNotEmpty(String str){
+		return !isEmpty(str);
+	}
+	
+	public static void print(Object parameter){
+		System.out.println("开始打印参数。");
+		System.out.println(parameter);
+	}
+	
+}
